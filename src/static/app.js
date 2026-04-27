41233c1861c97e8cfcc974022a8cbe2e6347ff61
Marcel Tsubota <marcel.tsu@hotmail.com>
2026-04-27 19:21:47 +0000

    Atualização para correção do exercício

--- src/static/app.js
+++ src/static/app.js
@@ -28,10 +28,17 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
-          <p><strong>Participants:</strong></p>
-          <ul>
-            ${details.participants.map(email => `<li>${email} <button class="delete-btn" data-activity="${name}" data-email="${email}">×</button></li>`).join('')}
-          </ul>
+          <div class="participants">
+            <h4>Participants:</h4>
+            <ul>
+              ${details.participants.map(email => `
+                <li>
+                  ${email}
+                  <button class="delete-btn" data-activity="${name}" data-email="${email}">×</button>
+                </li>
+              `).join('')}
+            </ul>
+          </div>
         `;
 
         activitiesList.appendChild(activityCard);
